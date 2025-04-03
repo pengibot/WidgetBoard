152eec6d0987d22e09b320f6dd5e06586c51e7ab
pengibot <pengibot@gmail.com>
2025-04-03 22:57:23 +0100

    Add project files.

---
+++ WidgetBoard/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace WidgetBoard
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+#if WINDOWS
+            CounterBtn.Text = $"Windows count: {count}";
+#elif ANDROID
+            CounterBtn.Text = $"Android count: {count}";
+#elif IOS
+            CounterBtn.Text = $"iOS count: {count}";
+#elif MACCATALYST
+            CounterBtn.Text = $"MacCatalyst count: {count}";
+#endif
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
