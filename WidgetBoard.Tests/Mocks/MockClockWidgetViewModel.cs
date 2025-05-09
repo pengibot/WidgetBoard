ef949a070c5f730038dab83b07ffcedb4eb28746
pengibot <pengibot@gmail.com>
2025-05-09 23:32:52 +0100

    Testing viewmodel is updated for ClockWidgetView. Page 438/455

---
+++ WidgetBoard.Tests/Mocks/MockClockWidgetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using WidgetBoard.ViewModels;
+namespace WidgetBoard.Tests.Mocks;
+
+public class MockClockWidgetViewModel : IWidgetViewModel
+{
+    public int Position { get; set; }
+    public string Type => "Mock";
+
+    public MockClockWidgetViewModel(DateTime time)
+    {
+        Time = time;
+    }
+
+    public DateTime Time { get; }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+}
